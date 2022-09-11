09c6d3ff332a07c22190ffcfffd33bf0b7397d69
Vladislav Dolganov <dolganoffprod@gmail.com>
2022-09-11 20:26:06 +0300

    3 ver

--- forjob/tdl-proj/scripts.js
+++ forjob/tdl-proj/scripts.js
@@ -3,16 +3,38 @@ window.addEventListener('load', () => {
   const input = document.querySelector('#new-task-input');
   const list_el = document.querySelector('#tasks', '#tasks-done');
 
+  let arr = [];
+  let count  = 0;
+  let active_count = 0;
+  let done_count = 0;
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
 
-    const tasks = input.value;
+    const tasks_test = input.value;
+
+    const tasks = tasks_test.trim()
+
+    
+
+    arr.forEach(elem => {
+      if(elem === tasks){
+        alert('Эта задача уже')
+        list_el.removeChild(tasks_el)
+      }
+    });
+
+    arr.push(tasks)
+
 
     if(!tasks) {
       alert("Введите задачу!");
       return;
     };
+    
+ 
+
 
 
     const tasks_el = document.createElement("div");
@@ -36,9 +58,7 @@ window.addEventListener('load', () => {
 
     tasks_content_el.appendChild(tasks_input_el)
 
-    if (tasks === tasks_input_el){
-      alert("Эта задача уже");
-    }
+
 
     const tasks_actions_el = document.createElement("div");
     tasks_actions_el.classList.add("actions", "acitons-done");
@@ -55,7 +75,6 @@ window.addEventListener('load', () => {
 		tasks_delete_el.classList.add('delete', 'delete-done');
 		tasks_delete_el.innerText = 'Delete';
 
-   
 
     tasks_actions_el.appendChild(tasks_done_el)
     tasks_actions_el.appendChild(tasks_edit_el)
@@ -64,15 +83,27 @@ window.addEventListener('load', () => {
     tasks_el.appendChild(tasks_actions_el);
 
     list_el.appendChild(tasks_el)
-    
+    count ++;
+    active_count ++;
+
 
     tasks_done_el.addEventListener('click', () => {
-
-      tasks_el.classList.remove('tasks');
-      tasks_input_el.classList.remove("text")
-      tasks_input_el.classList.add("text-done")
-      tasks_done_el.innerText = 'Cancel';
-
+      if(tasks_done_el.innerText.toLocaleLowerCase() == 'done'){
+        tasks_el.classList.remove('tasks');
+        tasks_input_el.classList.remove("text")
+        tasks_input_el.classList.add("text-done")
+        tasks_done_el.innerHTML = 'Cancel'
+        active_count --;
+        done_count ++;
+        
+      } else {
+        tasks_el.classList.add('tasks');
+        tasks_input_el.classList.add("text")
+        tasks_input_el.classList.remove("text-done")
+        tasks_done_el.innerHTML = 'Done'
+        done_count --;
+        active_count ++;
+      }
     })
 
     tasks_edit_el.addEventListener('click', () => {
@@ -80,9 +111,12 @@ window.addEventListener('load', () => {
         tasks_input_el.removeAttribute("readonly");
         tasks_input_el.focus();
         tasks_edit_el.innerText = "Save" 
+       
+
       } else {
         tasks_input_el.setAttribute("readonly", "readonly")
         tasks_edit_el.innerText = "Edit"
+        
       }
     })
 
@@ -100,9 +134,10 @@ window.addEventListener('load', () => {
 
     tasks_delete_el.addEventListener('click', () => {
       list_el.removeChild(tasks_el)
+      arr.splice(tasks)
+      active_count --;
+      count --;
+      done_count --;
     })
-
-
-
   })
 })
